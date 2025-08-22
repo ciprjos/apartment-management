2855ed0c9334b27494d9c54ab5dbb3c5dd3f5c2d
Jose Japhet <josejaphet.cipriano@outlook.com>
2025-08-23 02:22:19 +0800

    fix: remove outputFileTracingIncludes configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  outputFileTracingIncludes: {
-    "/": ["../../node_modules/argon2/prebuilds/linux-x64/*"],
-  },
 };
 
 export default nextConfig;
