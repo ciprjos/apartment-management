5b3856f14f2f24607aa0f73def494a8cb0180cd9
Jose Japhet <josejaphet.cipriano@outlook.com>
2025-08-22 17:02:18 +0800

    added initial UI for register

---
+++ app/(auth)/register/_components/form.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+import { GalleryVerticalEnd } from "lucide-react";
+
+export function RegisterForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <>
+      <div className={cn("flex flex-col gap-6", className)} {...props}>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col items-center gap-2">
+              <a
+                href="#"
+                className="flex flex-col items-center gap-2 font-medium"
+              >
+                <div className="flex size-8 items-center justify-center rounded-md">
+                  <GalleryVerticalEnd className="size-6" />
+                </div>
+                <span className="sr-only">Acme Inc.</span>
+              </a>
+              <h1 className="text-xl font-bold">Welcome to Acme Inc.</h1>
+            </div>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="lease-number">Lease Number</Label>
+                <Input
+                  id="lease-number"
+                  type="text"
+                  placeholder="Enter your lease number"
+                  required
+                />
+              </div>
+              <Button type="submit" className="w-full">
+                Proceed
+              </Button>
+            </div>
+          </div>
+        </form>
+        <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+          By clicking continue, you agree to our{" "}
+          <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,13 @@
+import { RegisterForm } from "./_components/form";
+
+export default function RegisterPage() {
+  return (
+    <>
+      <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+        <div className="w-full max-w-sm">
+          <RegisterForm />
+        </div>
+      </div>
+    </>
+  );
+}
