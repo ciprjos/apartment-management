ff2e61afc6171048c7af92aa3fba05781fb118ce
Jose Japhet <josejaphet.cipriano@outlook.com>
2025-08-24 04:57:16 +0800

    feat: implement role-based dashboard rendering for Admin, Landlord, and Tenant

--- app/portal/dashboard/page.tsx
+++ app/portal/dashboard/page.tsx
@@ -1,16 +1,27 @@
-import { ChartAreaInteractive } from "@/components/shared/chart-area-interactive";
-import { DataTable } from "@/components/shared/data-table";
-import { SectionCards } from "@/components/shared/section-cards";
+import { auth } from "@/auth";
+import { JSX } from "react";
+import { AdminDashboard } from "./_components/admin-dashboard";
+import { LandlordDashboard } from "./_components/landlord-dashboard";
+import { TenantDashboard } from "./_components/tenant-dashboard";
+import { ADMIN_ROLE, LANDLORD_ROLE, TENANT_ROLE } from "@/lib/constants";
 
-export default function DashboardPage() {
+type DashboardComponentType = () => JSX.Element;
+
+const RoleDashboardMap: Record<string, DashboardComponentType> = {
+  [ADMIN_ROLE]: AdminDashboard,
+  [LANDLORD_ROLE]: LandlordDashboard,
+  [TENANT_ROLE]: TenantDashboard,
+};
+export default async function DashboardPage() {
+  const session = await auth();
+
+  const DashboardComponent =
+    RoleDashboardMap[session!.user.role] ?? LandlordDashboard;
+
   return (
     <>
       <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-        <SectionCards />
-        <div className="px-4 lg:px-6">
-          <ChartAreaInteractive />
-        </div>
-        <DataTable data={[]} />
+        <DashboardComponent />
       </div>
     </>
   );
