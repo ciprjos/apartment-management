c77132dc37b35deb8be8dc2caa16f7a4a267a30e
Jose Japhet <josejaphet.cipriano@outlook.com>
2025-09-02 04:31:37 +0800

    refactor: simplify middleware logic for login and protected routes

--- middleware.ts
+++ middleware.ts
@@ -5,16 +5,13 @@ export const runtime = "nodejs";
 export async function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
   const session = await auth();
-
-  const isLoginPage = pathname.startsWith("/login");
-  const isProtected = pathname.startsWith("/portal");
   // If user is not logged in and tries to access protected page → redirect to /login
-  if (!session && isProtected) {
+  if (!session && pathname.startsWith("/portal")) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
   // If user is logged in and tries to access /login → redirect to dashboard
-  if (session && isLoginPage) {
+  if (session && !pathname.startsWith("/portal")) {
     return NextResponse.redirect(new URL("/portal/dashboard", request.url));
   }
 
@@ -22,5 +19,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/login/:path*", "/portal/:path*"], // login and protected routes
+  matcher: ["/", "/login", "/portal/:path*","/portal"], // login and protected routes
 };
