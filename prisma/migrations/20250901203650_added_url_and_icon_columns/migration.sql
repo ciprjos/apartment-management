4c2fccb08ed7347d702c0f6da3088ca76c37ed22
Jose Japhet <josejaphet.cipriano@outlook.com>
2025-09-02 04:37:44 +0800

    feat: add icon and url fields to Module and Submodule models

---
+++ prisma/migrations/20250901203650_added_url_and_icon_columns/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Module" ADD COLUMN     "icon" TEXT,
+ADD COLUMN     "url" TEXT;
+
+-- AlterTable
+ALTER TABLE "Submodule" ADD COLUMN     "icon" TEXT,
+ADD COLUMN     "url" TEXT;
